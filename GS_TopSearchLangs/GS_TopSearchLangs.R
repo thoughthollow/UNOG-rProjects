1401e92568dd778c4728f42afedf2034b5d859b1
Andy Tait <79048392+thoughthollow@users.noreply.github.com>
2022-02-05 20:57:22 +0100

    Create GS_TopSearchLangs.R
    Global Search top search languages for whatever period the data is from.
    Install R and RStudio to use it.
    
    Use this link to install R https://cran.r-project.org/bin/windows/base/
    Use this link to install R Studio https://www.rstudio.com/products/rstudio/download/

---
+++ GS_TopSearchLangs/GS_TopSearchLangs.R
@@ -0,0 +1,36 @@
+#Clear the working environment so as to start fresh. Can help to avoid weird errors i.e. it's best practice.
+rm(list=ls())
+
+#load the various packages. This must be done each time the script is run.
+#if the packages are not installed, then add beforehand for each package "install.packages("packagename")
+library(readr)
+library(cld2)
+library(tidyverse)
+library(stringr)
+
+#import the csv file data into R, and save it as a variable
+df <- read.csv("yourfilenameforimporting.csv")
+#rename the 3 columns
+names(df) <- c("search_string", "no_of_searches", "no_of_results_per_search")
+
+df <- mutate(df,  cld2_Lang = cld2::detect_language(search_string, lang_code = FALSE))
+df <- mutate(df, string_char_length = nchar(df$search_string))
+df <- mutate(df, no_of_words = str_count(df$search_string, "\\w+"))
+
+#detect the search language and add a column
+df <- mutate(df,  cld2_Lang = cld2::detect_language(search_string, lang_code = FALSE)) %>%
+
+#note that the %>% i.e. the pipe sends the data onto the next block of code, so no need to refer to "df" again
+#get the search string length as a column
+  mutate(string_char_length = nchar(df$search_string)) %>%
+
+#get the number of words in the search string as a column
+  mutate(no_of_words = str_count(df$search_string, "\\w+"))
+
+#the above is the same as this below:
+#df <- mutate(df,  cld2_Lang = cld2::detect_language(search_string, lang_code = FALSE))
+#df <- mutate(df, string_char_length = nchar(df$search_string))
+#df <- mutate(df, no_of_words = str_count(df$search_string, "\\w+"))
+
+#exort as csv, with  the datatime in the filename
+write.csv(df, paste0("GS_TopLangs_exported_", format(Sys.time(), "%Y-%m-%d_%H.%M"), ".csv"))
